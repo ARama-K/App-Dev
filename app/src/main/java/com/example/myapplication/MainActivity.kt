99959ef79843993551c020104f28ef55c6491f95
Aramak <81297962+ARama-K@users.noreply.github.com>
2024-06-20 15:01:42 +0530

    Assignment-4 (Login)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,46 @@
 package com.example.myapplication
-
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-    }
-    fun clickHandler(view: View) {
-        Log.i("MainActivity-clickhandler","button clicked")
-        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
-        var webIntent: Intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://www.ndtv.com"))
-        startActivity(webIntent)
-
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val inputField = findViewById<EditText>(R.id.editTextText)
+        val password = findViewById<EditText>(R.id.editTextTextPassword)
+        val submitButton = findViewById<Button>(R.id.button)
+        var enteredName = ""
+        var pass = ""
+        submitButton.setOnClickListener {
+            enteredName = inputField.text.toString()
+            pass = password.text.toString()
+            if (enteredName == ""){
+                Toast.makeText(this@MainActivity,"Please, Enter your name",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                if (pass == "1234") {
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("USER",enteredName)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this@MainActivity,"Incorrect Password",Toast.LENGTH_SHORT).show()
+                    password.text.clear()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val username = intent.getStringExtra("USER")
+        val offerMessage = findViewById<TextView>(R.id.offerText)
+        val message = "Hello $username, you will get access to all content for one month as your free trail"
+        offerMessage.text = message
+    }
+}
